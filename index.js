b9427b78265a012932edb26e9dc8a35534fa540f
Dan Krasilnikov <krasilneg@yandex.ru>
2021-03-07 19:58:36 +1000

    fix

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 module.exports = {
-
+  Calculator: require('./lib/Calculator'),
+  data: {
+    Item: require('./lib/datarepository/lib/Item'),
+    Property: require('./lib/datarepository/lib/Property'),
+    CachedDataRepository: require('./lib/datarepository/CachedDataRepository'),
+    SecuredDataRepository: require('./lib/datarepository/SecuredDataRepository'),
+    DataRepository: require('./lib/datarepository/ionDataRepository'),
+    utils: require('./lib/datarepository/lib/util')
+  },
+  meta: {
+    ClassMeta: require('./lib/meta/lib/ClassMeta'),
+    MongoDbSync: require('./lib/meta/mongo/dbSync'),
+    DsMetaRepository: require('./lib/meta/DsMetaRepository'),
+    KeyProvider: require('./lib/meta/keyProvider'),
+    WorkflowProvider: require('./lib/meta/WorkflowProvider'),
+    parseConditions: require('./lib/util/ConditionParser'),
+    dirNameGenerator: require('./lib/util/dirName')
+  },
+  errors: require('./lib/errors')
 };
